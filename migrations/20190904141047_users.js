53a2e7e0987f6527379aaab4d90313c8776a8016
Chubbard022 <chubbard022@gmail.com>
2019-09-05 13:18:48 -0700

    completed making tables for backend

--- migrations/20190904141047_users.js
+++ migrations/20190904141047_users.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable("users", user=>{
   
-};
-
-exports.down = function(knex) {
+          user.increments()
   
-};
+          user.string("username",50)
+              .notNullable()
+              .unique()
+          user.string("password",255)
+              .notNullable()
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("users")
+  };
+  
--- migrations/20190904141120_catigories.js
+++ migrations/20190904141120_catigories.js
@@ -1,8 +1,42 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("categories", category=>{
+
+        category.increments()//primary key
+
+        category.string("name",30)//name of category
+                .notNullable()
+            
+    })
+    .createTable("recipes", recipe=>{
+
+        recipe.increments()//primary key
+
+        recipe.string("name",50)//name of recipe
+            .notNullable()
+
+        recipe.string("instructions")//instructions to recipe
+            .notNullable()
+
+        recipe.string("time")//time of recipe prep
+            .notNullable()
+
+        recipe.string("ingredients")//ingredients to recipe
+
+        recipe.integer("category_id")//fk to category
+            .unsigned()
+            .references("id")
+            .inTable("categories")
+            .onDelete("RESTRICT")
+            .onUpdate("RESTRICT")
+    })
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("categories")
+
 };
