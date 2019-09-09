757a651ce77e5f81b6d36f8e0b1c09f78ab8f52e
Chubbard022 <chubbard022@gmail.com>
2019-09-09 12:38:29 -0700

    registering testing working on postman

--- config/routes.js
+++ config/routes.js
@@ -1,72 +1,74 @@
-const bcrpt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const {authenticate} = require("../auth/authenticate")
-const secret = require("../auth/secret")
-const Users = require("../auth/authenticate")
+const { authenticate } = require("../auth/authenticate");
+const secret = require("../auth/secret").jwtKey;
+const Users = require("../auth/authenticate");
 
-module.exports = server =>{
-    server.post("/api/register", register)
-    server.post("/api/login", login)
-    server.post("/api/logout", logout)
-}
+module.exports = server => {
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.post("/api/logout",logout)
+};
+function register(req, res) { 
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
 
-function register(req,res){
-    let user = req.body
-    const hash = bcrpt.hashSync(user.password,10)
-    user.password = hash
-
-    Users.add(user)
-    .then(users=>{
-        res.status(201).json(users)
+  Users.add(user)
+    .then(users => {
+      res.status(201).json(users);
     })
-    .catch(err=>{
-        res.status(500).json({errorMessage: "error with registering"})
+    .catch(err => {
+        console.log("******",err)
+      res.status(500).json({errorMessage:"error with registering"});
+    });
+}
+function login(req, res) {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ errorMessage: "Username and/or Password are incorrect" });
+      }
     })
+    .catch(error => {
+      res.status(500).json({errorMessage:"ERROR with logging in"});
+    });
 }
-
-function login(req,res){
-    let {username,password} = req.body
-
-    Users.findBy({username})
-        .then(user=>{
-            if(user && bcrpt.compareSync(password, user.password)){
-                const token = generateToken(user)
-
-                res.status(200).json({
-                    message: `Welcome ${user.username}`,
-                    token
-                })
-            }else{
-                res.status(401).json({errorMessage: "Error, username or password is incorrect"})
-            }
-        })
-        .catch(err=>{
-            res.status(500).json({errorMessage: "Error with logging in occurred "})
-        })
+function logout(req, res) {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res
+          .status(500)
+          .json({ errorMessage:'Error with trying to Logout'});
+      } else {
+        res.status(200).json({ message: 'logout successful' });
+      }
+    });
+  } else {
+    res.status(500).json({ errorMessage:'Error with trying to Logout' });
+  }
 }
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
 
-function logout(req,res){
-    if(req.session){
-        req.session.destroy(err=>{
-            if(err){
-                res.status(500).json({errorMessage: "Error with loggingout"})
-            }else{
-                res.status(200).json({message: "logout was successful"})
-            }
-        })
-    }else{
-        res.status(500).json({errorMessage: "Error with trying to logout"})
-    }
-}
+  const options = {
+    expiresIn: "1d"
+  };
 
-function generateToken(user){
-    const payload = {
-        subject: user.id,
-        username: user.username
-    }
-    const options = {
-        expiresIn: "id"
-    }
-    return jwt.sign(payload, secret, options)
+  return jwt.sign(payload, secret, options);
 }
